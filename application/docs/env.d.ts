81c7ec90a82c4041c3d76c998ac47a424236c394
la-niina <la.niina.me@gmail.com>
2025-07-29 05:12:50 +0300

    update

--- application/docs/env.d.ts
+++ application/docs/env.d.ts
@@ -1,6 +1,8 @@
 declare global {
     namespace NodeJS {
-        interface ProcessEnv extends CloudflareEnv { }
+        interface ProcessEnv extends CloudflareEnv {
+            GITHUB_TOKEN: string
+        }
     }
 }
 
